9f15602c1836f0542eb64fc4b0f171b93af46caf
Warren Reader <WarrenDReader@gmail.com>
2018-02-23 10:59:04 -0700

    Setup Massive

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// require('dotenv').config();
+const massive = require('massive');
+
+require('dotenv').config();
 
 const app = express();
 
 app.use(bodyParser.json());
 
 
-let PORT = process.env.PORT || 7777;
+let PORT = 7777;
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+massive(process.env.CONNECTION_STRING).then(db => {
+   app.set('db', db);
+   app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+})
